7586059ce207a537c88cf786ce2a947b1328d170
nsuami <2nsuami@gmail.com>
2022-11-15 17:10:54 -0500

    Fix Bug

--- front-end/src/layout/Layout.js
+++ front-end/src/layout/Layout.js
@@ -17,12 +17,19 @@ function Layout() {
         <div className="col-md-2 side-bar">
           <Menu />
         </div>
+        <div className="col" style={{ padding: "0px" }}>
+          <img
+            src="https://images.pexels.com/photos/299348/pexels-photo-299348.jpeg?cs=srgb&dl=pexels-terje-sollie-299348.jpg&fm=jpg&_gl=1*1tyxedn*_ga*MTI3NDg1Nzk3Mi4xNjY4NTQ1NTAx*_ga_8JE65Q40S6*MTY2ODU0NTUxMi4xLjEuMTY2ODU0ODE0OC4wLjAuMA.."
+            className="img-fluid"
+            alt="Dashboard banner"
+          ></img>
         <div className="col">
           <main>
             <Routes />
           </main>
         </div>
       </div>
+    </div>
     </div>
   );
 }
